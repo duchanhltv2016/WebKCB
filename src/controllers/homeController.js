90a693c34bad21b8645227730056d7e87e0ec8da
duchanhltv2016 <duchanhltv2016@gmail.com>
2022-11-20 23:13:21 +0700

    End to 28

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -28,17 +28,57 @@ let postCRUD = async (req, res) => {
 }
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------------');
-    console.log(data);
-    console.log('---------------------------');
     return res.render('test/displayCRUD.ejs', {
         dataTable: data,
     });
 }
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        console.log('----------');
+        console.log(userData);
+        console.log('----------');
+        if (userData) {
+            return res.render('test/editCRUD.ejs', {
+                user: userData,
+            });
+        }
+
+    }
+    else {
+        return res.send("Not found User");
+    }
+}
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('test/displayCRUD.ejs', {
+        dataTable: allUsers,
+    });
+}
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        let allUsers = await CRUDService.deleteUserById(id);
+        return res.render('test/displayCRUD.ejs', {
+            dataTable: allUsers,
+        });
+    }
+    else {
+        return res.send('User not found');
+    }
+
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
+    getEditCRUD: getEditCRUD,
     displayGetCRUD: displayGetCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
+
 }
