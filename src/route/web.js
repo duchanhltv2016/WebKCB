0b2c38116316b7e6025df646bd8b59720c562082
HanhND3 <hanh.nguyen@egitech.vn>
2023-03-18 10:20:35 +0700

    commit

--- src/route/web.js
+++ src/route/web.js
@@ -2,6 +2,12 @@ import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController"
 
+var cors = require('cors');
+var corsOptions = {
+    origin: 'http://localhost:3000/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 let router = express.Router();
 
 let initWebRoutes = (app) => {
